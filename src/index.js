fd6dc93e05e0690d9113c7831fde3ac5fbd38f84
EM\lsalunke <laxmi.salunke@elliemae.com>
2019-02-18 21:19:12 +0530

    Removed unwanted components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import angular from 'angular';
 
-import {TodoService} from './app/todos/todos';
+import {AppService} from './app/services/AppService';
 import {App} from './app/containers/App/App';
 import {Landing} from './app/containers/Landing/Landing';
-import {Header} from './app/components/Header/Header';
-import {MainSection} from './app/components/MainSection/MainSection';
-import {TodoTextInput} from './app/components/TodoTextInput/TodoTextInput';
-import {TodoItem} from './app/components/TodoItem/TodoItem';
-import {Footer} from './app/components/Footer/Footer';
 import 'angular-ui-router';
 import routesConfig from './routes';
 
@@ -16,11 +11,6 @@ import './index.scss';
 angular
   .module('app', ['ui.router'])
   .config(routesConfig)
-  .service('todoService', TodoService)
+  .service('appService', AppService)
   .component('app', App)
-  .component('landing', Landing)
-  .component('headerComponent', Header)
-  .component('footerComponent', Footer)
-  .component('mainSection', MainSection)
-  .component('todoTextInput', TodoTextInput)
-  .component('todoItem', TodoItem);
+  .component('landing', Landing);
